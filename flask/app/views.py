34f5063254fc757a01bf8314ebaff796bf55ab8b
ice <ksyukon@outlook.com>
2019-05-09 23:34:23 +0700

    edit callback return

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,7 @@
 from app import app
 import requests
-from flask import render_template_string, redirect, request
+from flask import render_template_string, redirect, request, jsonify
+import json
 
 @app.route('/')
 @app.route('/index')
@@ -28,13 +29,13 @@ def login():
 def callback():
     code = request.args.get('code')
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    req = requests.post('https://notify-bot.line.me/oauth/token', data={
+    res = requests.post('https://notify-bot.line.me/oauth/token', data={
         "grant_type": "authorization_code",
         "code": code,
         "redirect_uri": "https://notify.npworld.info/callback/",
         "client_id": "2FiajbKaqThu1rRS8CdJYM",
         "client_secret": "vTDF9Wi1tWzhM79PQpSp7pqDUhoK6Bx5jF8vGDOD9l0"
-    }, headers=headers)
-    return req['access_token']
+    }, headers=headers).json()
+    return jsonify(res)
     
 
